a179cd585c611be2fce01fc16c806a1d057b7c71
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-11 15:01:33 +0100

    Remove DEBUG=true; replace hab pkg exec with full path to binary; parse the pkg_version from installation path; use File.join() for path building
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/bison_exists.rb
+++ controls/bison_exists.rb
@@ -2,12 +2,12 @@ title 'Tests to confirm bison exists'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'bison')
- 
+
 control 'core-plans-bison-exists' do
   impact 1.0
   title 'Ensure bison exists'
   desc '
-  Verify bison by ensuring /bin/bison exists'
+  Verify bison by ensuring bin/bison exists'
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
@@ -15,8 +15,8 @@ control 'core-plans-bison-exists' do
     its('stdout') { should_not be_empty }
   end
 
-  command_relative_path = input('command_relative_path', value: '/bin/bison')
-  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  command_relative_path = input('command_relative_path', value: 'bin/bison')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
   describe file(command_full_path) do
     it { should exist }
   end
