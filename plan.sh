b3bc629364de5bffb6e3d176032b148667afaee2
RaviDuddela <RaviShankar.Duddela@progress.com>
2021-10-27 13:49:13 +0530

    version bump from 3.7.6 to 3.8.2
    Signed-off-by: RaviDuddela <RaviShankar.Duddela@progress.com>

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 pkg_name=bison
 pkg_origin=core
-pkg_version=3.7.6
+pkg_version=3.8.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 Bison is a general-purpose parser generator that converts an annotated \
@@ -10,7 +10,7 @@ employing LALR(1) parser tables.\
 pkg_upstream_url="https://www.gnu.org/software/bison/"
 pkg_license=('GPL-3.0')
 pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum="67d68ce1e22192050525643fc0a7a22297576682bef6a5c51446903f5aeef3cf"
+pkg_shasum="9bba0214ccf7f1079c5d59210045227bcf619519840ebfa80cd3849cff5a5bf2"
 pkg_deps=(
   core/glibc
   core/m4
